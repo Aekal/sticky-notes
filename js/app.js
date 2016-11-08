0b7d68e3738ef28ee872bdf39a21e15aa22b6e67
Damian Fiałkiewicz <aekal4@gmail.com>
2016-11-09 00:29:33 +0100

    Add checkboxes and colors

--- js/app.js
+++ js/app.js
@@ -1,28 +1,104 @@
 $(document).ready(function() {
-	var myCard = {
-		init : function() {
-			this.cacheDOM();
-			this.display();
-		},
-		cacheDOM : function() {
-			this.$container = $("#cards-container");
-		},
-		display : function() {
-			this.newCard = $("<div/>", {
-				class: "card",
-			}).appendTo(this.$container);
 
-			this.minusBtn = $("<button/>", {
-				type: "button",
-				class: "minus fa fa-minus",
-				click: function() {
-					$(this).parent().remove();
-				}
-			}).appendTo(this.newCard);
-		},
-	};
+function createCard() {
+	var $container = $("#cards-container");
 
-	$("#btn-create").on("click", function() {
-		myCard.init();
-	});
+	var newCard = $("<div/>", {
+		class: "card",
+		click: function(e) {
+			if ($(e.target).is(".card")) {
+				$(this).find("input").focus();
+			}
+		}
+	}).appendTo($container);
+
+	var minusCardBtn = $("<button/>", {
+		type: "button",
+		class: "btn-minus fa fa-minus",
+		click: function() {
+			$(this).parent().remove();
+		}
+	}).appendTo(newCard);
+
+	var list = $("<ul/>", {class: "list"}).appendTo(newCard);
+
+	var inputField = $("<input/>", {
+		class: "new-elem",
+		type: "text",
+		blur: function() {
+			//createListItem()
+		},
+		keypress: function(e) {
+			if((e.which == 13) && ($(this).val() !== "")) {
+				var list = $(this).parent().find(".list");
+
+				var item = $("<li/>", {
+					class: "item",
+					click: function (e) {
+						$(this).find("span").toggleClass("selected");
+
+						var checkbox = $(this).find(".checkbox")
+						if (checkbox.hasClass("fa-square-o")) {
+							checkbox
+							.removeClass("fa-square-o")
+							.addClass("fa-check-square-o")
+						} else {
+							checkbox
+							.removeClass("fa-check-square-o")
+							.addClass("fa-square-o")
+						}
+					}
+				}).appendTo(list);
+
+				var checkbox = $("<div/>",{
+					class: "checkbox fa fa-square-o",
+				}).appendTo(item);
+
+				var itemText = $("<span/>", {
+					text: $(this).val()
+				}).appendTo(item);
+
+				var minusItemBtn = $("<button/>", {
+					type: "button",
+					class: "fa fa-minus-circle",
+					click: function() {
+						$(this).parent().remove();
+					}
+				}).appendTo(item);
+
+				$(this).val("");
+			}
+		}
+	}).appendTo(newCard);
+
+	var colorList = $("<ul/>", {
+		class: "card-colors"
+	}).appendTo(newCard);
+
+	var colorItems = [];
+	for (i = 0; i < 3; i++) {
+		colorItems.push(
+			$("<li/>", {
+				class: "color"
+			}).appendTo(colorList)
+		);
+	}
+	$(".color").click(function () {
+		var selectedColor = $(this).css("background-color");
+		$(this).closest(".card").css("background-color", selectedColor);
+	})
+ }
+
+ $(".card").on("click", function(e) {
+	 if ($(e.target).is(".card")) {
+		 $(this).find("input").focus();
+	 }
+ });
+
+ $("#btn-create").on("click", function() {
+	 createCard();
+ });
+
+ createCard();
+
 });
